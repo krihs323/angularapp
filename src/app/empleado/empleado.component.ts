d9440f41c75a3e3df62520f0db615b7f240ee9be
cristianbotina89 <cristian_botina@hotmail.com>
2019-02-27 23:09:21 -0500

    ejercicio Two way data-binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -11,6 +11,8 @@ export class EmpleadoComponent{
   public empleado:Empleado;
   public trabajadores:Array<Empleado>;
   public trabajador_externo:boolean;
+  public color:string;
+  public color_seleccionado:string;
 
   constructor(){
     this.empleado = new Empleado('David Lopez',45,'Cocinero',true);
@@ -18,8 +20,11 @@ export class EmpleadoComponent{
        new Empleado('Manolo',20,'Cocinero',true),
        new Empleado('Ana',18,'Programador',false),
        new Empleado('Victor',45,'Mesero',true),
+       new Empleado('Cristian Leandro Botina',29,'Analista',true),
     ];
     this.trabajador_externo = true;
+    this.color = 'red';
+    this.color_seleccionado = '#CCC';
   }
 
   ngOnInit(){
