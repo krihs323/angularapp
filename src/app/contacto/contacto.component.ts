817875c45e3c5bf628579eca01d1bb7d6a4e0680
CRISTIAN LEANDRO BOTINA CAIPE <CRISTIAN.BOTINA@LAN.GRUPOSSI.COM>
2019-03-12 13:18:23 -0500

    routing y redireccion con parametros

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'contacto',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
   public titulo = 'Contacto de la web';
-  constructor() { }
+  public parametro;
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {}
 
   ngOnInit() {
+    this._route.params.forEach((params: Params) => {
+        this.parametro = params['page'];
+        console.log(params);
+    });
+  }
+
+  redirigir(){
+    this._router.navigate(['/contacto','cristianbotina']);
+  }
+  redirigirHome(){
+    this._router.navigate(['/pagina-principal']);
   }
 
 }
