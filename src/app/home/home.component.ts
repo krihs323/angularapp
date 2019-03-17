349f4fb57c1cd25e1faaf9b55b43b0e390f6323e
CRISTIAN LEANDRO BOTINA CAIPE <CRISTIAN.BOTINA@LAN.GRUPOSSI.COM>
2019-03-16 21:32:20 -0500

    ejercicio de servicios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { RopaService } from '../services/ropa.service';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[RopaService]
 })
 export class HomeComponent implements OnInit {
   public titulo='Pagina Principal' ;
-  constructor() { }
+  constructor(
+    private _ropaService: RopaService
+  ) { }
 
   ngOnInit() {
+    console.log(this._ropaService.prueba('camisa puma'));
   }
 
 }
