28c7695764b9e2af473f4d7f159903c7c07fcbd5
cristianbotina89 <cristian_botina@hotmail.com>
2019-03-16 22:59:14 -0500

    Ejercicio de Servicios y formulario reactivo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { RopaService } from '../services/ropa.service';
 
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[RopaService]
 })
 export class HomeComponent implements OnInit {
   public titulo='Pagina Principal' ;
-  constructor() { }
+  public listado_ropa:Array<string>;
+  public prenda_a_guardar:string;
+
+  constructor(
+    private _ropaService: RopaService
+  ) { }
 
   ngOnInit() {
+    this.listado_ropa = this._ropaService.getRopa();
+    console.log(this._ropaService.prueba('camisa puma'));
+    console.log(this.listado_ropa);
+    
+  }
+
+  guardarPrenda(){
+    this._ropaService.addRopa(this.prenda_a_guardar);
+    this.prenda_a_guardar = null;
+  }
+
+  eliminarPrenda(index:number){
+    this._ropaService.deleteRopa(index);
+    
   }
 
 }
